1063bf63d69f7595c57a339800f0abe7de75ca45
Andres Felipe <afm0325@gmail.com>
2025-10-06 08:39:21 -0500

    Organizing the app packages

--- app/src/main/java/com/andrew/alatreon/ui/activity/MainActivity.kt
+++ app/src/main/java/com/andrew/alatreon/ui/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.andrew.alatreon.ui
+package com.andrew.alatreon.ui.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.andrew.alatreon.R
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
