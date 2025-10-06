2fa8e10f48b6eff7dc802b2fd492631a35f2b28f
Andres Felipe <afm0325@gmail.com>
2025-10-06 13:33:48 -0500

    viewBinding added

--- app/src/main/java/com/andrew/alatreon/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/andrew/alatreon/ui/activity/LoginActivity.kt
@@ -6,13 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.andrew.alatreon.R
+import com.andrew.alatreon.databinding.ActivityLoginBinding
 import com.andrew.alatreon.util.Logger
 
 class LoginActivity : AppCompatActivity(), Logger {
+    //Adding the viewBinding variable
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        //Inflating the viewBinding variable
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/andrew/alatreon/ui/activity/MainActivity.kt
+++ app/src/main/java/com/andrew/alatreon/ui/activity/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.andrew.alatreon.R
+import com.andrew.alatreon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Adding the viewBinding variable
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        //Inflating the viewBinding variable
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
